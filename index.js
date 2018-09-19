fcf50344952b57a8a7ae9728615cdeb568df8a58
Dhruv <DhruvProtocol@gmail.com>
2018-09-19 19:40:48 +0530

    Folder structure

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-const chalk = require('chalk');
-const figlet = require('figlet');
+import chalk from 'chalk';
+import figlet from 'figlet';
+import files from './lib/files';
+import clear from 'clear';
+import questions from './lib/questions';
+import cli from './lib/cli';
 
 // clear();
+
 console.log(
   chalk.yellow(
-    figlet.textSync('ELI', { horizontalLayout: 'full' })
+    figlet.textSync('ExCLI', { horizontalLayout: 'full' })
   )
 );
+
+console.log(chalk.green(`
+  A CLI for Express 
+`));
+
+(async () => {
+  try {
+    let name = await questions.getName()
+    let type = await questions.getTypes();
+    let view = await questions.getView();
+    await cli(Object.assign({}, type, view, name))    
+  } catch (error) {
+    console.error(error)
+  }
+
+})();
